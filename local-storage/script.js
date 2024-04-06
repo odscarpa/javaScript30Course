c3a8b9e7022c6ba3d6b6b5ba8a06498dd959fe5a
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-04-06 11:11:34 -0600

    wrote function to populate the list with data from input and store it in local storage

--- local-storage/script.js
+++ local-storage/script.js
@@ -1,6 +1,6 @@
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-const items = [];
+const addItems = document.querySelector('.add-items')
+const itemsList = document.querySelector('.plates')
+const items = JSON.parse(localStorage.getItem('items')) || []
 
 function addItem(e) {
 e.preventDefault()
@@ -11,7 +11,22 @@ e.preventDefault()
     }
 
     items.push(item)
+    populateList(items, itemsList)
+    localStorage.setItem('items', JSON.stringify(items))
     this.reset()
 }
 
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+        <li>
+        <input type="checkbox" data-index="${i}" id="item${i}" ${plate.done ? 'checked' : ''} />
+        <label for="item${i}">${plate.text}</label>
+        </li>
+        `
+    }).join('')
+}
+
 addItems.addEventListener('submit', addItem)
+
+populateList(items, itemsList)
