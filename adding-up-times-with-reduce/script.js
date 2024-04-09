8bd63d93bb77875509e1bf2c52979c612ba6b0a6
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-04-09 14:49:58 -0600

    Implement JavaScript code to calculate hours, minutes, and remaining seconds from total seconds

--- adding-up-times-with-reduce/script.js
+++ adding-up-times-with-reduce/script.js
@@ -7,3 +7,11 @@ const seconds = timeNodes
         return (mins * 60) + secs
     })
     .reduce((total, vidSeconds) => total + vidSeconds)
+
+let secondsLeft = seconds
+const hours = Math.floor(secondsLeft / 3600)
+secondsLeft = secondsLeft % 3600
+
+const mins = Math.floor(secondsLeft / 60)
+secondsLeft = secondsLeft % 60
+console.log(hours, mins, secondsLeft)
