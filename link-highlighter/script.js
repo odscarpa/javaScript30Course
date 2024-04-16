9e2382b50bdbe668c53b84c6314669a430fc7fc3
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-04-15 23:05:50 -0600

    Fixed highlight size calculation and positioning. Corrected the highlight effect code to accurately set the width and height of the highlight element when hovering over links. Also adjusted the positioning to ensure proper alignment with the hovered link.

--- link-highlighter/script.js
+++ link-highlighter/script.js
@@ -6,9 +6,17 @@ document.body.append(highlight)
 function highlightLink() {
     const linkCoords = this.getBoundingClientRect()
     console.log(linkCoords)
-    highlight.style.width = `${linkCoords.width}px`
-    highlight.style.width = `${linkCoords.height}px`
 
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX
+    }
+
+    highlight.style.width = `${coords.width}px`
+    highlight.style.height = `${coords.height}px` // Corrected line
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`
 }
 
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink))
