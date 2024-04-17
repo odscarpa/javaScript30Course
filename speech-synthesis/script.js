69ba8541ca41ba26ac6234e018e9872b31ff3438
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-04-16 23:21:13 -0600

    this function dynamically generates and updates the options available in a dropdown menu based on the voices available for speech synthesis

--- speech-synthesis/script.js
+++ speech-synthesis/script.js
@@ -1,6 +1,15 @@
-const msg = new SpeechSynthesisUtterance();
-let voices = [];
-const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector('#speak');
-const stopButton = document.querySelector('#stop');
+const msg = new SpeechSynthesisUtterance()
+let voices = []
+const voicesDropdown = document.querySelector('[name="voice"]')
+const options = document.querySelectorAll('[type="range"], [name="text"]')
+const speakButton = document.querySelector('#speak')
+const stopButton = document.querySelector('#stop')
+msg.text = document.querySelector('[name="text"]').value
+
+function populateVoices() {
+    voices = this.getVoices();
+    voicesDropdown.innerHTML = voices
+        .filter(voice => voice.lang.includes('en'))
+        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+        .join('');
+}
