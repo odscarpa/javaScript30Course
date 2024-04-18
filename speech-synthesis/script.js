5e3c5103016f9e92c4070b15d0713188d2ae8486
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-04-17 21:01:20 -0600

    Attach event listeners for dynamic options and speech synthesis controls

--- speech-synthesis/script.js
+++ speech-synthesis/script.js
@@ -34,3 +34,6 @@ function setOption() {
 
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
 voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
