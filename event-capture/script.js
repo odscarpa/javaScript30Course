8d42a0a029b8c1ce1da7753a2c1953bc4c818f2a
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-04-21 21:02:02 -0600

    initial set up

--- event-capture/script.js
+++ event-capture/script.js
@@ -2,7 +2,7 @@ const divs = document.querySelectorAll('div')
 const button = document.querySelector('button')
 
 function logText(e) {
-    console.log(this.classList.value);
+    console.log(this.classList.value)
     // e.stopPropagation(); // stop bubbling!
     // console.log(this);
 }
