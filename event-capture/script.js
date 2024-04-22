3ec5c4b13da97347c9b016f86dbe675b8afc6d3f
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-04-21 20:50:13 -0600

    Added a one-time 'click' event listener to the button element, logging 'Clickgit push !' upon click

--- event-capture/script.js
+++ event-capture/script.js
@@ -11,3 +11,9 @@ divs.forEach(div => div.addEventListener('click', logText, {
     capture: false,
     once: true
 }))
+
+button.addEventListener('click', () => {
+    console.log('Click!!!');
+}, {
+    once: true
+})
