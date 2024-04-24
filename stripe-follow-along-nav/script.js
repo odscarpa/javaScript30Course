62a204f5a3ccfe7f3b33b7ca2af6a6d0af0255f5
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-04-23 18:41:18 -0600

    Added function to handle navigation trigger hover effects and dropdown activation with a delay

--- stripe-follow-along-nav/script.js
+++ stripe-follow-along-nav/script.js
@@ -1,3 +1,9 @@
 const triggers = document.querySelectorAll('.cool > li')
 const background  = document.querySelector('.dropdownBackground')
 const nav  = document.querySelector('.top')
+
+function handleEnter() {
+    this.classList.add('trigger-enter');
+    setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+    background.classList.add('open');
+}
