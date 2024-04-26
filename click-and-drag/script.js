ed98f17da882c98d36eefe988c4ad560330d8608
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-04-26 16:26:59 -0600

    Implement mouse event listeners for slider interaction

---
+++ click-and-drag/script.js
@@ -0,0 +1,20 @@
+const slider = document.querySelector('.items')
+let isDown = false
+let startX
+let scrollLeft
+
+slider.addEventListener('mousedown', (e) => {
+
+})
+
+slider.addEventListener('mouseleave', () => {
+
+})
+
+slider.addEventListener('mouseup', () => {
+
+})
+
+slider.addEventListener('mousemove', (e) => {
+
+})
