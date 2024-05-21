3fa0b50bffd14c0046e1903835a9f7ecb0cd6346
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-05-21 15:25:47 -0400

    Add peep function for mole popping animation

--- whack-a-mole/script.js
+++ whack-a-mole/script.js
@@ -1,13 +1,30 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false
 
-function randTime(min, max) {
+function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
 function randomHole(holes) {
     const idx = Math.floor(Math.random() * holes.length);
     const hole = holes[idx];
-    console.log(hole)
+    if(hole === lastHole) {
+        console.log('same hole big crip')
+        return randomHole(holes)
+    }
+    lastHole = hole
+    return hole
+}
+
+function peep() {
+    const time = randomTime(200, 1000)
+    const hole = randomHole(holes)
+    hole.classList.add('up')
+    setTimeout(() => {
+        hole.classList.remove('up')
+        if(!timeUp) peep()
+    }, time)
 }
