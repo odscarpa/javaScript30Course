34b8ab25e03a8e9801abcbf9192a32aeb1f7ba38
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-05-21 15:42:15 -0400

    Add startGame function to initiate the game

--- whack-a-mole/script.js
+++ whack-a-mole/script.js
@@ -28,3 +28,10 @@ function peep() {
         if(!timeUp) peep()
     }, time)
 }
+
+function startGame() {
+    scoreBoard.textContent = 0
+    timeUp = false
+    peep()
+    setTimeout(() => timeUp = true, 10000)
+}
