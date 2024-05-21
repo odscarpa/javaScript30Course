f6d5cf353f49979fc647700b2d5f269fff7f295b
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-05-21 15:50:18 -0400

    Add bonk function to handle mole clicks

--- whack-a-mole/script.js
+++ whack-a-mole/script.js
@@ -3,6 +3,7 @@ const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
 let timeUp = false
+let score = 0
 
 function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -32,6 +33,15 @@ function peep() {
 function startGame() {
     scoreBoard.textContent = 0
     timeUp = false
+    score = 0
     peep()
     setTimeout(() => timeUp = true, 10000)
 }
+
+function bonk(e) {
+    if(!e.isTrusted) return; // NO CHEATERS!! (no fake mouse clicks)
+    score++
+    this.classList.remove('up')
+    scoreBoard.textContent = score
+}
+moles.forEach(mole => mole.addEventListener('click', bonk))
